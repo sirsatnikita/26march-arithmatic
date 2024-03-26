e3c06da4134b5f28b63943fd8c9f0e0f8f8425fc
sirsatnikita <140889181+sirsatnikita@users.noreply.github.com>
2024-03-26 10:21:51 +0530

    Add files via upload

---
+++ assignment .js
@@ -0,0 +1,8 @@
+//1. Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.
+ let a = 23;
+ let b=45 ;
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a%b);
+ console.log(a/b);
+ console.log(a*b);
